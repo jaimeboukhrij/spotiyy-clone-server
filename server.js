9deffb2c38164bece9fefabd58e286c7ddb84dd7
jaime <jaime.boukhrij@smartick.com>
2024-04-22 18:46:22 +0200

    j

--- server.js
+++ server.js
@@ -1,9 +1,7 @@
 const app = require("./app");
 
-// Define el puerto en el que deseas que tu aplicación escuche
-const PORT = 3005;
+const PORT = process.env.PORT || 3005;
 
-// Configura tu aplicación para que escuche en todas las direcciones IP en el puerto especificado
 app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server listening on http://0.0.0.0:${PORT}`);
 });
